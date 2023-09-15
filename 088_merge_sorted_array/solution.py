2f2a02cf0c1bdfa30c6999b7c92efc44e12d56d3
Evgenii Stepanchenko <pobugi@gmail.com>
2023-09-15 19:48:46 +0200

    088 - merge sorted array

---
+++ 088_merge_sorted_array/solution.py
@@ -0,0 +1,26 @@
+nums1 = [2, 7, 8, 0, 0, 0]
+m = 3
+nums2 = [1, 5, 6]
+n = 3
+
+
+def solve(nums1: list, m: int, nums2: list, n: int) -> list:
+    last = m + n - 1
+    while m > 0 and n > 0:
+        if nums1[m - 1] > nums2[n - 1]:
+            nums1[last] = nums1[m - 1]
+            m -= 1
+        else:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+        last -= 1
+
+    while n > 0:
+        nums1[last] = nums2[n - 1]
+        n -= 1
+        last -= 1
+
+    return nums1
+
+
+print(solve(nums1, m, nums2, n))
