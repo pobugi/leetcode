f37ca1e7639d27c4eae4792168560c6e5a1d74fd
Evgenii Stepanchenko <pobugi@gmail.com>
2023-11-20 21:10:04 +0100

    225 - implement stack using queues

---
+++ 225_implement_stack_using_queues/solution.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def empty(self) -> bool:
+        return not self.stack
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
