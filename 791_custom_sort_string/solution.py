a8f31d2dd7ad9852ebc5e81e44c973a0af732c27
Evgenii Stepanchenko <pobugi@gmail.com>
2024-03-11 09:18:07 +0100

    791 - custom sort string: optimize

--- 791_custom_sort_string/solution.py
+++ 791_custom_sort_string/solution.py
@@ -5,21 +5,24 @@ s = "abcd"
 
 
 class Solution:
-    def customSortString(self, s: str, order: str) -> str:
+    def customSortString(self, order: str, s: str) -> str:
         result = ""
         s_dict = dict()
         for letter in s:
             s_dict[letter] = s_dict.get(letter, 0) + 1
         for letter in order:
+            if letter in s_dict and s_dict[letter] == 0:
+                del s_dict[letter]
             while s_dict.get(letter, 0) > 0:
                 result += letter
                 s_dict[letter] -= 1
+
         for letter in s_dict:
-            qty = s_dict[letter]
-            if qty > 0:
-                result += letter * qty
+            while s_dict[letter] > 0:
+                result += letter
+                s_dict[letter] -= 1
 
         return result
 
 
-print(Solution().customSortString(s, order))
+print(Solution().customSortString(order, s))
