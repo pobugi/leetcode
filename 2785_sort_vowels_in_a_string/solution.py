fe858bec08b6f4183f49a53c4051f68cf39f095a
Evgenii Stepanchenko <pobugi@gmail.com>
2024-03-31 21:43:33 +0200

    2785 - sort vowels in a string: optimized

--- 2785_sort_vowels_in_a_string/solution.py
+++ 2785_sort_vowels_in_a_string/solution.py
@@ -1,14 +1,23 @@
+from collections import defaultdict
+
+
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-        vowels_list = []
-
+        vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+        vowels_dict = defaultdict(int)
         for letter in s:
             if letter in vowels:
-                vowels_list.append(letter)
-        vowels_list.sort()
+                vowels_dict[letter] += 1
+
         for i in range(len(s)):
             if s[i] in vowels:
-                vowel = vowels_list.pop(0)
-                s = s[:i] + vowel + s[i + 1 :]
+                for vowel in vowels:
+                    if vowels_dict[vowel] > 0:
+                        replacement = vowel
+                        vowels_dict[replacement] -= 1
+                        s = s[:i] + replacement + s[i + 1 :]
+                        break
         return s
+
+
+print(Solution().sortVowels("lEetcOde"))
