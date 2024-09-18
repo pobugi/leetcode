7e33cd8647cceb7ad590bef4d11055d246596fae
Evgenii Stepanchenko <pobugi@gmail.com>
2024-09-18 11:48:22 +0200

    112 - path sum: go solution

---
+++ 112_path_sum/solution.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil && targetSum == 0 {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
+
+func main() {
+	return
+}
