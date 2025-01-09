282d411a5f38b373edefdd08ddf7e8f9c04535cc
Evgenii Stepanchenko <pobugi@gmail.com>
2025-01-09 10:37:16 +0100

    2695 - array wrapper: ts solution (fix)

--- 2695_array_wrapper/solution.ts
+++ 2695_array_wrapper/solution.ts
@@ -17,6 +17,7 @@ class ArrayWrapper {
 
 const obj1 = new ArrayWrapper([1, 2]);
 const obj2 = new ArrayWrapper([3, 4]);
-console.log(obj1);
+console.log(Number(obj1) + Number(obj2));
+console.log(String(obj1) + String(obj2));
 console.log(obj1.valueOf() + obj2.valueOf());
 console.log(obj1.toString(), obj2.toString());
